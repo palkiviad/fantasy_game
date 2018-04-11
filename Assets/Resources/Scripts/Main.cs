85fb2c42e14f2a10b5ddcaae16c37777e5a4cde9
alkiviad <ppopov_2010@mail.ru>
2018-04-12 00:17:45 +0300

    create project

---
+++ Assets/Resources/Scripts/Main.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Main : MonoBehaviour
+{
+    // Use this for initialization
+    protected void Start()
+    {
+        Debug.Log("Hello my fantasy game!");
+        StartCoroutine(Upload());
+    }
+
+    IEnumerator Upload()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add( new MultipartFormDataSection("name=authorize") );
+        UnityWebRequest www = UnityWebRequest.Post("http://localhost:8080", formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        
+        Debug.Log("SERVER SAYS : " + www.downloadHandler.text);
+    }
+}
